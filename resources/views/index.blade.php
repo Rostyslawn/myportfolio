e002d1e17fe6cc3796217bf5ed66dc0348c00c77
Rostyslaw <rostyslawn02@gmail.com>
2024-06-14 15:36:56 +0300

    refactoring(new main-info)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,47 +7,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{asset('imgs/icon.png')}}">
-    <title>Rostyslaw</title>
+    <title>Portfolio</title>
 </head>
 <body>
-<div class="nav">
-    <div class="name">
-        Rostyslav Humeniuk
+<div class="main-info">
+    <div class="nav">
+        <div class="home"><a href="{{route("index")}}">Home</a></div>
+        <div class="projects"><a>Projects</a></div>
+        <div class="reviews"><a>Reviews</a></div>
+        <div class="getintouch"><a>Get In Touch</a></div>
     </div>
-    <div class="items">
-        <a href="{{route('index')}}" class="item home">Home</a>
-        <a href="#projects" class="item home">My projects</a>
-        <a class="item about">About me</a>
-        <a class="item contact">Contact</a>
-    </div>
-</div>
-<div class="main">
-    <div class="container">
-        <h1>My Portfolio</h1>
-        <div class="containerAboutMe">
-            <div class="aboutMe"></div>
-            <span class="blinking-cursor"></span></div>
-    </div>
-</div>
-<div class="projects">
-    <h1 id="projects">My Projects</h1>
-    <div class="container">
-        <div class="project project-1">
-            <div class="image">
-                <img alt="photo" src="{{asset('imgs/myphoto.jpg')}}">
-            </div>
-            <div class="aboutProject">
-                <h2>Project 1</h2>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eй commodo consequat. Duis aute irure dolor in reprehendent in voluptate velit</span>
-            </div>
+    <div class="main">
+        <div class="left-side">
+            <h1>Rostyslav Humeniuk</h1>
+            <span class="about-me">
+                Intro text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </span>
+            <button class="start">Let's get started</button>
         </div>
-        <div class="project project-2">
-            <div class="image">
-                <img alt="photo" src="{{asset('imgs/myphoto.jpg')}}">
-            </div>
-            <div class="aboutProject">
-                <h2>Project 2</h2>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eй commodo consequat. Duis aute irure dolor in reprehendent in voluptate velit</span>
+        <div class="right-side">
+            <div class="photo">
+                <img src="{{asset('imgs/icon.png')}}" alt="my-photo">
             </div>
         </div>
     </div>
